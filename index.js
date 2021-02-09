e8e2942873eeb35ec8d0a4e458fdc2d9edd9e87b
claire <clairebontempo@gmail.com>
2021-02-08 20:05:18 -0800

    commit tree 8b2e507103e86704110acc1297fbd4c9728a6809

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(array, action) {
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(action(element))
+    })
+    return newArray
+ }
+    
+
+ function reduce(sourceArray, action, start){
+    let returnValue = (!!start) ? start : sourceArray[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < sourceArray.length; i++) {
+      returnValue = action(sourceArray[i], returnValue)
+    }
+  
+    return returnValue;
+  }
+  
